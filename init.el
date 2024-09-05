190993f2d005af1630a7b241fa632ef661ccfd92
sam <3161728@gmail.com>
2024-09-05 17:49:57 +0900

    Change evil quit command

--- init.el
+++ init.el
@@ -12,7 +12,7 @@
   (message "Your Emacs is old, and some functionality in this config will be disabled. Please upgrade if possible."))
 
 ;; Turn off mouse interface early in startup to avoid momentary display
-(if (fboundp 'menu-bar-mode) (menu-bar-mode 1))
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 
--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -58,8 +58,8 @@
 
 ;; :q should kill the current buffer rather than quitting emacs entirely
 (evil-ex-define-cmd "q" 'kill-this-buffer)
-;; Need to type out :quit to close emacs
-(evil-ex-define-cmd "quit" 'evil-quit)
+;; Need to type out :qq to close emacs
+(evil-ex-define-cmd "qq" 'evil-quit)
 
 ;; Make evil-mode up/down operate in screen lines instead of logical lines
 (define-key evil-motion-state-map "j" 'evil-next-visual-line)
